fa8b5f9f2a082b097dc16c29513ad30e983330b1
Calum <calumjamescraig@gmail.com>
2025-03-22 17:01:35 +0000

    Added high score

--- alien_invasion.py
+++ alien_invasion.py
@@ -64,25 +64,31 @@ class AlienInvasion:
         """Start a new game when the player clicks Play"""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)       
         if button_clicked and not self.stats.game_active:
+            #Reset game settings
             self.settings.initialise_dynamic_settings()
-            self._start_game()
-
-    def _start_game(self):
-        #Reset the game statistics
+            #Reset the game statistics
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
 
-            #Get rid of any remaining aliens and bullets
-            self.aliens.empty()
-            self.bullets.empty()
+            self._start_game()
 
-            #Create a new fleet and centre the ship
-            self._create_fleet()
-            self.ship.center_ship()
+    def _start_game(self):
+        #Reset the game statistics
+        self.stats.reset_stats()
+        self.stats.game_active = True
+        self.sb.prep_score()
 
-            #Hide the mouse cursor
-            pygame.mouse.set_visible(False)
+        #Get rid of any remaining aliens and bullets
+        self.aliens.empty()
+        self.bullets.empty()
+
+        #Create a new fleet and centre the ship
+        self._create_fleet()
+        self.ship.center_ship()
+
+        #Hide the mouse cursor
+        pygame.mouse.set_visible(False)
 
     def _check_keydown_events(self, event):
         """Respond to keypresses"""
@@ -128,8 +134,10 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
         if collisions:
-            self.stats.score += self.settings.alien_points
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             #Destroy existing bullets and create new fleet
--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ class Settings:
         self.bg_color = (230, 230, 230)
         
         #Ship settings
-        self.ship_limit = 3
+        self.ship_limit = 1
 
         #Bullet settings
         self.bullet_width = 500
@@ -22,6 +22,9 @@ class Settings:
         
         #How quickly the game speeds up
         self.speedup_scale = 1.1
+
+        #How quickly the alien point values increase
+        self.score_scale = 1.1
 
         self.initialise_dynamic_settings()
 
@@ -38,8 +41,9 @@ class Settings:
         self.alien_points = 50
 
     def increase_speed(self):
-        """Increase speed settings"""
+        """Increase speed settings and alien point values"""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+        self.alien_points *= int(self.alien_points * self.score_scale)
         
